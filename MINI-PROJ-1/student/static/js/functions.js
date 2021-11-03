ed986e6204616b88f87f3ef6d81f8f1b0e06ee20
BallsAngus <jmtuazon09@gmail.com>
2021-11-03 04:22:30 -0700

    added intro

--- MINI-PROJ-1/student/static/js/functions.js
+++ MINI-PROJ-1/student/static/js/functions.js
@@ -6,7 +6,63 @@
 //     console.log("The button has been clicked");
 // }
 
-// Todo:
-// - Add js that uses DOM
+var hidden = true;
 
-// Your code here
+document.getElementById("projects").addEventListener("click", function () {
+    console.log("clicked");
+    hidden = !hidden;
+
+    const stuff = document.getElementsByClassName("intro-container");
+    const other_stuff = document.getElementsByClassName("project-container");
+    const btn = document.getElementById("projects");
+    console.log(stuff);
+    if (!hidden) {
+        //shift(stuff, "container-visible", "container-hidden")
+        btn.innerHTML = "Back";
+        for (let i = 0; i < stuff.length; i++) {
+            console.log(stuff[i]);
+            stuff[i].classList.add("container-hidden");
+            stuff[i].classList.remove("container-visible");
+            stuff[i].classList.remove("container");
+
+        }
+
+        for (let i = 0; i < other_stuff.length; i++) {
+            other_stuff[i].classList.add("container");
+            other_stuff[i].classList.add("container-visible");
+            other_stuff[i].classList.remove("container-hidden");
+            other_stuff[i].classList.remove("test");
+
+        }
+
+
+    } else {
+        //shift(stuff, "container-hidden", "container-visible")
+        btn.innerHTML = "Cool Stuff?";
+        for (let i = 0; i < stuff.length; i++) {
+            console.log(stuff[i]);
+            stuff[i].classList.add("container-visible");
+            stuff[i].classList.add("container");
+            stuff[i].classList.remove("container-hidden");
+
+        }
+
+        for (let i = 0; i < other_stuff.length; i++) {
+            other_stuff[i].classList.remove("container");
+            other_stuff[i].classList.remove("container-visible");
+            other_stuff[i].classList.add("container-hidden");
+            other_stuff[i].classList.add("test");
+
+
+        }
+    }
+})
+
+function shift(elem, before, after) {
+
+    for (let i = 0; i < elem.length; i++) {
+        elem[i].classList.add(after);
+        elem[i].classList.remove(before);
+
+    }
+}
--- MINI-PROJ-1/student/views.py
+++ MINI-PROJ-1/student/views.py
@@ -7,9 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    pass
-    # Your code here
-
+    return render_template("your_code.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
